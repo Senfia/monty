1e95bcfea171c0a2a51a242fd6cef6e55b607139
Senyo Fiawornu <contactsenyo@gmail.com>
2023-05-29 09:59:35 +0000

    edited comments

--- rncode.c
+++ rncode.c
@@ -8,8 +8,6 @@ opret_q ret_and_q = {0, 0};
  * @stack: Double pointer to the top of the stack
  * @line_: The line number where the command was found
  *
- * Return: 1 if no command is found, return opcode_return if command was
- * found. (opcode_return is changed by the opcodes, if they fail)
  **/
 int check_codes(char *command, stack_t **stack, size_t line_)
 {
